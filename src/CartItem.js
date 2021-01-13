6f26f6d7a3ce5e45d2474323ebee63c8a27b19ee
shubham200 <shubhamms2210@gmail.com>
2021-01-14 01:23:52 +0530

    added a cart component

--- src/CartItem.js
+++ src/CartItem.js
@@ -6,44 +6,63 @@ class CartItem extends React.Component {
         super();
         this.state = {
             price:11999,
-            title:'(refurbished)OnePlus 5',
+            title:'OnePlus 5',
             qty:1,
             img:''
         }
     }
 
     increaseQuantity=() => {
-         this.state.qty++;
          console.log(this.state.qty)
+         this.setState((prevState) => {
+             return {
+                 qty : prevState.qty+1
+             }
+         })
+    }
+
+    decreaseQuantity=() => {
+        const {qty} = this.state
+        if(qty===0)
+        {
+            return;
+        }
+        console.log(this.state.qty)
+        this.setState((prevState) => {
+            return {
+                qty:prevState.qty -1
+            }
+        })
     }
     render() {
         const {price,title,qty} = this.state;
         return (
          <div className="cart-item">
          <div className="left-block">
-            <img style={styles.image} />
+            <img style={styles.image} src="https://static.toiimg.com/photo/59267498.cms" />
          </div>
          <div className="right-block">
          <div style={{fontSize:25}}>{title}</div>
-         <div style={{fontSize:18,Color:'#777'}}>Rs{price}</div>
-         <div style={{fontSize:18,Color:'#777'}}>Qty:{qty}</div>
+         <div style={{fontSize:18,Color:'#777'}}>Rs. {price}</div>
+         <div style={{fontSize:18,Color:'#777'}}>Qty: {qty}</div>
          <div className="cart-item-actions">
             {/*buttons*/}
             <img
             className="action-icons" 
-            alt="increase" 
-            src="https://www.flaticon.com/svg/vstatic/svg/1828/1828926.svg?token=exp=1610490574~hmac=31628f72bcb1cd459806d5c1e50e5501"
+            alt="+" 
+            src="https://www.flaticon.com/svg/vstatic/svg/159/159690.svg?token=exp=1610500754~hmac=0f2a3d90319f57df5d45608bcf22ec5f"
             onClick={this.increaseQuantity}
             />
             <img 
             className="action-icons" 
-            alt="decrease"
-            src="https://www.flaticon.com/svg/vstatic/svg/1828/1828906.svg?token=exp=1610490624~hmac=fa18e511543f971e9f7a3085092d5c51"
+            alt="-"
+            src="https://www.flaticon.com/svg/vstatic/svg/1828/1828906.svg?token=exp=1610500881~hmac=e88e3934057e245f28e27042ad698f37"
+            onClick={this.decreaseQuantity}
             />
             <img 
             className="action-icons" 
-            alt="delete" 
-            src="https://www.flaticon.com/svg/vstatic/svg/1345/1345823.svg?token=exp=1610490705~hmac=21f6204e3c121bb16b5b5d8fb0e12f64"
+            alt="x" 
+            src="https://www.flaticon.com/svg/vstatic/svg/3209/3209887.svg?token=exp=1610500957~hmac=dedeb2a886a08871cf7e5cf341084e3f"
             />
             </div>
          </div>
